9f3b72958f5970a0d1922405c9ec4e2280fb722a
Chuxin Huang <chuxin@canva.com>
2021-12-18 11:20:41 +1100

    simplify macro and conform to style guide

--- macros/incremental_utils.sql
+++ macros/incremental_utils.sql
@@ -1,4 +1,4 @@
 {% macro incremental_load_filter(time_col) -%}
     -- dbt makes it easy to query your target table by using the "{{ this }}" variable.
-    WHERE {{time_col}} > (select max({{time_col}}) from {{ this }})
+    where {{ time_col }} > (select max({{ time_col }}) from {{ this }})
 {%- endmacro %}
