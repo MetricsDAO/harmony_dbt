c2a3b8ea09178c6224a62a78ab94320dc80ab24b
drakedanner <drake.danner@gmail.com>
2021-12-22 13:46:59 -0500

    remove formatting and comments

--- models/core/blocks.sql
+++ models/core/blocks.sql
@@ -1,35 +1,17 @@
-{{ config(
-    materialized = 'incremental',
-    unique_key = 'block_id',
-    tags = ['core']
-) }}
+{{ config(materialized='incremental', unique_key='block_id', tags=['core']) }}
 
-SELECT
+select 
     block_id,
     block_timestamp,
-    header :hash :: STRING AS block_hash,
-    header :parent_hash :: STRING AS block_parent_hash,
-    header :gas_limit AS gas_limit,
-    header :gas_used AS gas_used,
-    header :miner :: STRING AS miner,
-    header :size AS SIZE,
+    header:hash::string as block_hash,
+    header:parent_hash::string as block_parent_hash,
+    header:gas_limit as gas_limit,
+    header:gas_used as gas_used,
+    header:miner::string as miner,
+    header:size as size,
     tx_count
-<<<<<<< HEAD
-FROM
-    {{ deduped_blocks("harmony_blocks") }}
-    -- Incrementaly load new data so that we don't do a full refresh each time
-    -- we run `dbt run` see the macro `macros/incremental_utils.sql`
-    -- or https://docs.getdbt.com/docs/building-a-dbt-project/building-models/configuring-incremental-models
-WHERE
-    {{ incremental_load_filter("block_timestamp") }}
-    -- hey
-    -- adding stuff
-=======
 from {{ deduped_blocks("harmony_blocks") }}
 -- Incrementaly load new data so that we don't do a full refresh each time
 -- we run `dbt run` see the macro `macros/incremental_utils.sql` 
 -- or https://docs.getdbt.com/docs/building-a-dbt-project/building-models/configuring-incremental-models
 where {{ incremental_load_filter("block_timestamp") }}
--- test
--- another test, yo!
->>>>>>> main
