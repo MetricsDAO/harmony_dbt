73c6a0f05084dc1f628415696a960fe1d08e63e6
Chuxin Huang <chuxin@canva.com>
2021-12-24 11:35:49 +1100

    update blocks sql and yml

--- models/core/blocks.sql
+++ models/core/blocks.sql
@@ -1,4 +1,10 @@
-{{ config(materialized='incremental', unique_key='block_id', tags=['core']) }}
+{{ 
+    config(
+        materialized='incremental', 
+        unique_key='block_id', 
+        tags=['core']
+    ) 
+}}
 
 select 
     block_id,
@@ -8,10 +14,11 @@ select
     header:gas_limit as gas_limit,
     header:gas_used as gas_used,
     header:miner::string as miner,
+    header:nonce::string as nonce,
     header:size as size,
-    tx_count
+    tx_count,
+    header:state_root::string as state_root,
+    header:receipts_root::string as receipts_root
 from {{ deduped_blocks("harmony_blocks") }}
--- Incrementaly load new data so that we don't do a full refresh each time
--- we run `dbt run` see the macro `macros/incremental_utils.sql` 
--- or https://docs.getdbt.com/docs/building-a-dbt-project/building-models/configuring-incremental-models
 where {{ incremental_load_filter("block_timestamp") }}
+order by block_timestamp
