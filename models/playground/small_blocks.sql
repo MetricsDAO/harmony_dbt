73bc7ceeca69a7503469e32d7d5fd026de866bd6
Anton Yip <Anton_Yip@hotmail.com>
2021-12-25 11:44:26 +0800

    updates

--- models/playground/small_blocks.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='incremental', unique_key='block_id', tags=['playground', 'small_blocks', 'small']) }}
-select
-    *
-from harmony.dev.blocks q
-where {{ incremental_load_filter("block_timestamp") }}
--- models/playground/small_txs.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='incremental', unique_key='tx_id', tags=['playground', 'small_txs', 'small']) }}
-select
-    *
-from harmony.dev.txs q
-where {{ incremental_load_filter("block_timestamp") }}
