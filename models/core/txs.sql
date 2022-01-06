d1ad2cef50d4f8626fe5d8da62c462099f4dc073
mattromano <42412983+mattromano@users.noreply.github.com>
2022-01-06 07:28:20 -0800

    spacing and header lowercase format

--- models/core/txs.sql
+++ models/core/txs.sql
@@ -8,18 +8,18 @@
 select
     block_timestamp,
     tx:nonce::string as nonce,
-    tx_block_index as "INDEX",
-    tx :bech32_from :: string as native_from_address,
-    tx :bech32_to :: string as native_to_address,
-    tx :from :: string as from_address,
-    tx :to :: string as to_address,
-    tx :value as "VALUE",
-    tx :block_number as block_number,
-    tx :block_hash :: string as block_hash,
-    tx :gas_price as gas_price,
-    tx :gas as gas,
+    tx_block_index as index,
+    tx:bech32_from::string as native_from_address,
+    tx:bech32_to::string as native_to_address,
+    tx:from::string as from_address,
+    tx:to::string as to_address,
+    tx:value as value,
+    tx:block_number as block_number,
+    tx:block_hash::string as block_hash,
+    tx:gas_price as gas_price,
+    tx:gas as gas,
     tx_id as "HASH",
-    tx :input :: string as "DATA",
-    tx:receipt:status::string = '0x1'  as "STATUS"
+    tx :input::string as data,
+    tx:receipt:status::string = '0x1'  as status
 from {{ deduped_txs("harmony_txs") }}
 where {{ incremental_load_filter("block_timestamp") }}
