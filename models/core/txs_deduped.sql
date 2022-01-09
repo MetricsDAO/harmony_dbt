493102f5ff329bac9fc355da8c8d4a69141a45c4
Anton Yip <anton_yip@hotmail.com>
2022-01-09 13:58:52 +0800

    review: fixes

--- models/core/txs_deduped.sql
+++ models/core/txs_deduped.sql
@@ -7,11 +7,13 @@
         )
 }}
 
-with deduped_raw_txs as (
+with
+
+deduped_raw_txs as (
 
     select 
-    *
-    from {{ source("chainwalkers", "harmony_txs" ) }}
+        *
+    from {{ source("chainwalkers","harmony_txs") }}
     where {{ incremental_load_filter("block_timestamp") }}
     qualify row_number() over (partition by tx_id order by ingested_at desc) = 1
 
