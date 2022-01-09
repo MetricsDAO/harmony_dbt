dc02ecdc79e2a218654d20110fdaea8610510a16
Anton Yip <anton_yip@hotmail.com>
2022-01-09 11:28:46 +0800

    checkpoint

---
+++ models/core/blocks_deduped.sql
@@ -0,0 +1,20 @@
+{{ 
+    config(
+        materialized='incremental',
+        unique_key='block_id',
+        tags=['core'],
+        cluster_by=['block_timestamp']
+        )
+}}
+
+with deduped_raw_blocks as (
+
+    select 
+    *
+    from {{ source("chainwalkers", "harmony_blocks" ) }}
+    where {{ incremental_load_filter("block_timestamp") }}
+    qualify row_number() over (partition by block_id order by ingested_at desc) = 1
+
+)
+
+select * from deduped_raw_blocks
