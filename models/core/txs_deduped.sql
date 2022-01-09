99ad667afab07fcb4e4b27bcbd32ae597811f06f
Anton Yip <anton_yip@hotmail.com>
2022-01-09 11:56:17 +0800

    feat: dbt for dedup blocks and txs

---
+++ models/core/txs_deduped.sql
@@ -0,0 +1,20 @@
+{{ 
+    config(
+        materialized='incremental',
+        unique_key='tx_id',
+        tags=['core'],
+        cluster_by=['block_timestamp']
+        )
+}}
+
+with deduped_raw_txs as (
+
+    select 
+    *
+    from {{ source("chainwalkers", "harmony_txs" ) }}
+    where {{ incremental_load_filter("block_timestamp") }}
+    qualify row_number() over (partition by tx_id order by ingested_at desc) = 1
+
+)
+
+select * from deduped_raw_txs
