030cae378684c4f3ab40863164cfc74a48b76eb1
ndhung1104 <ndhung1104@gmail.com>
2022-01-13 10:19:03 +0700

    Resolve conversations

--- models/core/transfers.sql
+++ models/core/transfers.sql
@@ -1,24 +1,37 @@
-{{ config(
-  materialized='incremental', 
-  unique_key= "CONCAT_WS('-', 'block_id', 'tx_hash', coalesce('from_address',''), coalesce('to_address',''), coalesce('contract_address',''), coalesce('raw_amount',-1))",
-  incremental_strategy = 'delete+insert',
-  cluster_by = ['block_timestamp'],
-  tags=['core']
-) }}
+{{ 
+    config(
+        materialized='incremental', 
+        unique_key= "CONCAT_WS('-', 'log_id', 'block_id', 'tx_hash', coalesce('from_address',''), coalesce('to_address',''), coalesce('contract_address',''), coalesce('raw_amount',-1))",
+        incremental_strategy = 'delete+insert',
+        tags=['core'],
+        cluster_by = ['block_timestamp']
+    ) 
+}}
 
-with logs as (
+with 
+
+logs as (
+
   select 
-    q.block_id, 
-    q.block_timestamp, 
-    q.tx_hash,
-    q.evm_contract_address, 
-    q.event_name, 
-    q.event_inputs
-  from 
-    {{ ref('logs') }} q
+
+    log_id,
+    block_id, 
+    block_timestamp, 
+    tx_hash,
+    evm_contract_address, 
+    event_name, 
+    event_inputs
+
+  from {{ ref('logs') }}
+  where {{ incremental_load_filter("block_timestamp") }}
+
 ),
+
 transfers as (
+
   select
+
+    log_id,
     block_id,
     tx_hash,
     block_timestamp,
@@ -26,15 +39,11 @@ transfers as (
     event_inputs:from::string as from_address,
     event_inputs:to::string as to_address,
     event_inputs:value::float as raw_amount
-  from 
-    logs
-  where 
-    event_name::string = 'Transfer'
-  and 
-    event_inputs:value is not null
-) 
-select 
-  *
-from 
-  transfers
 
+  from logs
+  where event_name::string = 'Transfer'
+  and event_inputs:value is not null
+
+) 
+
+select * from transfers
