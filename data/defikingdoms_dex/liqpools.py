5cf6f4ae2abc3e233542700fbfc3f2c0f2505b9b
Anton Yip <anton_yip@hotmail.com>
2022-01-14 08:24:39 +0800

    clear out unneeded code

--- data/defikingdoms_dex/liqpools.py
+++ data/defikingdoms_dex/liqpools.py
@@ -6,19 +6,6 @@ y = json.loads(f.read())
 
 dups = []
 
-def replaceQuotes(buf):
-    return buf.replace("'","")
-
-doOnceFlag = True
-def doOnce(firstTime, repeatedTimes):
-    global doOnceFlag
-    if doOnceFlag == True:
-        doOnceFlag = False
-        return firstTime
-    return repeatedTimes
-
-#print("{{ config(materialized='table', unique_key=\"CONCAT_WS('-', pool_address, pool_name)\", tags=['playground', 'ant_labels', 'ant_dex_lp_labels']) }}")
 print("pool_address,pool_name,token0,token1")
 for lpPool in y["data"]["pairs"]:
-    #print(doOnce("      ","union ") + "select '" + lpPool["id"] + "' as pool_address, '" + lpPool["token0"]["symbol"] + "-" + lpPool["token1"]["symbol"] + " LP" + "' as pool_name, '" + lpPool["token0"]["id"] + "' as token0, '" + lpPool["token1"]["id"] + "' as token1")
     print(lpPool["id"]+","+lpPool["token0"]["symbol"]+"-"+lpPool["token1"]["symbol"]+" LP"+","+lpPool["token0"]["id"]+","+lpPool["token1"]["id"])
