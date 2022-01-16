9af315f405efe34c3ab0770070fcb516db498d90
Anton Yip <anton_yip@hotmail.com>
2022-01-16 09:28:21 +0800

    fix: python script

--- data/defikingdoms_dex/tokens.py
+++ data/defikingdoms_dex/tokens.py
@@ -7,15 +7,15 @@ dups = []
 
 print("token_address,token_name,token_symbol,decimals")
 for lpPool in data["data"]["pairs"]:
-    if (lpPool["token0"]["id"] in dups) == False:
+    if not (lpPool["token0"]["id"] in dups):
         print(lpPool["token0"]["id"]+","+lpPool["token0"]["name"]+","+lpPool["token0"]["symbol"]+","+lpPool["token0"]["decimals"])
         dups.append(lpPool["token0"]["id"])
         
-    if (lpPool["token1"]["id"] in dups) == False:
+    if not (lpPool["token1"]["id"] in dups):
         print(lpPool["token1"]["id"]+","+lpPool["token1"]["name"]+","+lpPool["token1"]["symbol"]+","+lpPool["token1"]["decimals"])
         dups.append(lpPool["token1"]["id"])
 
 ## Additional tables -- if you need it 
-#if ('0x9edb3da18be4b03857f3d39f83e5c6aad67bc148' in dups) == False:
+#if not ('0x9edb3da18be4b03857f3d39f83e5c6aad67bc148' in dups):
 #    print("0x9edb3da18be4b03857f3d39f83e5c6aad67bc148,Golden Egg,TMP_DFKGOLDEGG,0)
 
