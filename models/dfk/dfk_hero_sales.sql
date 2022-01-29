47c9030a0618080f1676cbc7a54b3d6636431a73
Anton Yip <anton_yip@hotmail.com>
2022-01-29 11:15:10 +0800

    use kakamora's WIP for stats

--- models/dfk/dfk_hero_sales.sql
+++ models/dfk/dfk_hero_sales.sql
@@ -2,17 +2,23 @@
      config(
          materialized = 'incremental',
          unique_key = 'tx_hash',
-         tags=['dfk', 'dfk_quest_rewards']
+         incremental_strategy = 'delete+insert',
+         tags=['dfk'],
          cluster_by = ['block_timestamp']
      ) 
 }}
-with logs as (
+
+with
+
+logs as (
     select 
         *
     from {{ ref('logs') }}
     where {{ incremental_load_filter("block_timestamp") }}
-), hero_auction_txns as (
-    SELECT 
+),
+
+hero_auction_txns as (
+    select 
         tx_hash,
         block_timestamp,
         java_hextoint(replace(topics[1]::string, '0x','' )) as hero_token_id,
@@ -23,23 +29,25 @@ with logs as (
         concat('0x',LTRIM(substr(data, 3+128, 64),'0')) as buyer_address,
         -1 as total_usd, -- TODO: Calculate it        
         -1 as tax_usd -- TODO: Calculate it        
-    FROM logs
-    WHERE event_removed = false
+    from logs
+    where event_removed = false
         and evm_contract_address = '0x13a65b9f8039e2c032bc022171dc05b30c3f2892'
         and topics[0]::string ='0xe40da2ed231723b222a7ba7da994c3afc3f83a51da76262083e38841e2da0982' -- AuctionSuccessful event
-        and block_timestamp > '2021-12-20' -- TODO: Remove it
+),
+
+final as (
+    select 
+        tx_hash,
+        block_timestamp,
+        hero_token_id,
+        auction_id,
+        total_jewels,
+        tax_jewels,
+        total_usd,
+        tax_usd,
+        seller_address,
+        buyer_address
+    from hero_auction_txns
 )
-SELECT 
-    tx_hash,
-    block_timestamp,
-    hero_token_id,
-    auction_id,
-    total_jewels,
-    tax_jewels,
-    total_usd,
-    tax_usd,
-    seller_address,
-    buyer_address
-FROM hero_auction_txns
-ORDER BY block_timestamp desc -- TODO: Remove it
-LIMIT 100 -- TODO: Remove it
+
+select * from final
