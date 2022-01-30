0df58b82380f56ea7d8e3d45b8cd91e90e3685e4
Anton Yip <anton_yip@hotmail.com>
2022-01-31 00:49:38 +0800

    fix: upgrade hero level up to new version without ant_logs

--- models/dfk/dfk_hero_levelup.sql
+++ models/dfk/dfk_hero_levelup.sql
@@ -1,7 +1,7 @@
 {{ 
     config(
         materialized='incremental',
-        unique_key='tx_id',
+        unique_key='tx_hash',
         incremental_strategy = 'delete+insert',
         tags=['dfk'],
         cluster_by=['block_timestamp']
@@ -12,11 +12,11 @@ with
 
 level_up_txs_start as (
     select 
-        tx_hash as tx_id,
+        tx_hash,
         java_hextoint(substr(data,3+8,64))::integer as hero_id,
         block_timestamp,
         from_address as hero_owner
-    from harmony.prod.txs
+    from {{ ref("txs") }}
     where to_address = '0x0594d86b2923076a2316eaea4e1ca286daa142c1'
         and substr(data,0,10)= '0xfa863736'
 ),
@@ -24,19 +24,19 @@ level_up_txs_start as (
 sum_raw_level_ups as (
     select 
         block_timestamp,
-        tx_id,
-        eth_contract_address,
+        tx_hash,
+        evm_contract_address,
         sum(event_inputs:value) as amount
-    from harmony.dev.ant_logs
-    where tx_id in (select tx_id from level_up_txs_start)
-        and event_emitted_name = 'Transfer'
+    from {{ ref("logs") }}
+    where tx_hash in (select tx_hash from level_up_txs_start)
+        and event_name = 'Transfer'
     group by 1,2,3
 ),
 
 final as (
     select 
         m.block_timestamp,
-        m.tx_id,
+        m.tx_hash,
         m.hero_id,
         m.hero_owner,
         srune.amount as rune_amount,
@@ -46,11 +46,11 @@ final as (
         rune_price_usd + jewel_price_usd as amount_usd
     from level_up_txs_start as m
     left join sum_raw_level_ups srune 
-        on srune.tx_id = m.tx_id
-        and srune.eth_contract_address = '0x66f5bfd910cd83d3766c4b39d13730c911b2d286'
+        on srune.tx_hash = m.tx_hash
+        and srune.evm_contract_address = '0x66f5bfd910cd83d3766c4b39d13730c911b2d286'
     left join sum_raw_level_ups sjewel
-        on sjewel.tx_id = m.tx_id
-        and sjewel.eth_contract_address = '0x72cb10c6bfa5624dd07ef608027e366bd690048f'
+        on sjewel.tx_hash = m.tx_hash
+        and sjewel.evm_contract_address = '0x72cb10c6bfa5624dd07ef608027e366bd690048f'
     left join harmony.dev.tokenprice_jewel sjewelprice
         on date(sjewelprice.timestamp) = date(m.block_timestamp)
     left join harmony.dev.tokenprice_shvasrune sruneprice
