0ac613b239b7b5f9c791828e58318c79633f765a
tonykau1 <25996568+tonykau1@users.noreply.github.com>
2022-02-19 15:17:03 -0800

    pulled out tranquil markets tokenprice table for easier re-use

--- models/core/tranquil_txs.sql
+++ models/core/tranquil_txs.sql
@@ -22,21 +22,8 @@ tranquil_contracts as (
 ), 
 
 tranquil_markets_usd as (  
-    select 'ONE' as token_symbol, * from tokenprice_one
-    union all
-    select 'stONE' as token_symbol, * from tokenprice_stone
-    union all
-    select '1WBTC' as token_symbol, * from tokenprice_btc
-    union all
-    select '1BTC' as token_symbol, * from tokenprice_btc
-    union all
-    select '1ETH' as token_symbol, * from tokenprice_eth
-    union all
-    select '1USDC' as token_symbol, * from tokenprice_usd
-    union all
-    select '1USDT' as token_symbol, * from tokenprice_usd
-    union all
-    select '1DAI' as token_symbol, * from tokenprice_usd
+    select * 
+    from {{ ref('tranquil_markets_tokenprice') }}
 ),
 
 tranquil_deposits as (
