d127b46ead21919eb8a3f279a8e621c2eb404d40
Chuxin Huang <chuxin@canva.com>
2022-02-23 10:22:41 +1100

    update to style guide

--- models/core/btc_bridge.sql
+++ models/core/btc_bridge.sql
@@ -32,25 +32,29 @@ transfers as (
     *
   from  {{ ref('transfers') }}
   where {{ incremental_load_filter("block_timestamp") }}
+),
+final as (
+  select 
+    t.log_id,
+    t.block_id,
+    t.tx_hash,
+    t.block_timestamp,
+    t.contract_address,
+    t.from_address,
+    t.to_address,
+    case 
+      when t.from_address = '0x0000000000000000000000000000000000000000' then 'issue'
+      when t.to_address = '0x0000000000000000000000000000000000000000' then 'redeem'
+    end as tx_type,
+    v.vault_address is not null as is_vault,
+    t.raw_amount
+  from transfers as t 
+  left join vaults as v 
+    on t.to_address = v.vault_address
+  where 
+    t.contract_address = '0xdc54046c0451f9269fee1840aec808d36015697d' -- HMY_ONE_BTC_CONTRACT
+    and (t.from_address = '0x0000000000000000000000000000000000000000'  -- issue
+        or t.to_address = '0x0000000000000000000000000000000000000000') -- redeem
 )
-select 
-  t.log_id,
-  t.block_id,
-  t.tx_hash,
-  t.block_timestamp,
-  t.contract_address,
-  t.from_address,
-  t.to_address,
-  case 
-    when t.from_address = '0x0000000000000000000000000000000000000000' then 'issue'
-    when t.to_address = '0x0000000000000000000000000000000000000000' then 'redeem'
-  end as tx_type,
-  v.vault_address is not null as is_vault,
-  t.raw_amount
-from transfers as t 
-left join vaults as v 
-  on t.to_address = v.vault_address
-where 
-  t.contract_address = '0xdc54046c0451f9269fee1840aec808d36015697d' -- HMY_ONE_BTC_CONTRACT
-  and (t.from_address = '0x0000000000000000000000000000000000000000'  -- issue
-       or t.to_address = '0x0000000000000000000000000000000000000000') -- redeem
+
+select * from final
