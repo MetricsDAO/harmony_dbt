6ea38229b64fee3affdc9ad226afb20520d63786
tonykau1 <25996568+tonykau1@users.noreply.github.com>
2022-03-07 00:10:34 -0800

    Add swaps-based pricing table

--- models/tokenprice/tokenprice_from_swaps.sql
+++ models/tokenprice/tokenprice_from_swaps.sql
@@ -1,26 +1,34 @@
 {{
     config(
         materialized='table',
-        unique_key="token_address||'-'||block_date",
+        unique_key="key",
         tags=['core', 'defi', 'tokenprice'],
         cluster_by=['block_date', 'token_address']
         )
 }}
 
--- TODO: currently has duplicate volume - limit joins?
--- TODO: price discrepancy for 1ETH '0x6983d1e6def3690c4d616b13597a09e6193ea013' on Jan 24 - maybe more
+-- unique_key="token_address||'-'||block_date",
+
+-- TODO: price discrepancy for 1ETH '0x6983d1e6def3690c4d616b13597a09e6193ea013' on Jan 24 
+    -- small swaps through USDC extrapolated to all volume - need to prioritize highest volume pairs for accurate pricing
 --     suspected: 1USDC / AVAX pool: 0x422a9ff25a356525b8188c3c7074e0b0a345279d
+
+-- TODO: -- remove gross mispriced swaps?
+    -- where s.tx_hash not in ( -- remove gross mispriced swaps
+    --'0x45306aade61a002fff4bf42b68edb48addfb821c0f5d373201f3ab33b8d4abb4',
+    --'0x8c0b1638bf8f9e093b70d12faa5e639818e25902860fa1e0a6c422e6e3ccadfe'
 
 with 
 stables as (
   select * from {{ ref('harmony_stable_tokens')}}
 ),
 
--- trim the swaps table, truncate to day, and normalize the decimals
+-- trim the swaps table, truncate to day, and normalize the decimals, remove nulls with inner join
 simpleswaps as (
   select
     date_trunc('day', s.block_timestamp) as block_date,
     s.tx_hash,
+    s.pool_address,
     s.token0_address,
     s.token0_symbol,
     s.token1_address,
@@ -28,20 +36,17 @@ simpleswaps as (
     (s.amount0in + s.amount0out) / pow(10, t0.decimals) as amt0,
     (s.amount1in + s.amount1out) / pow(10, t1.decimals) as amt1
   from {{ ref('swaps') }} as s
-  left join {{ ref('tokens') }} as t0
+  inner join {{ ref('tokens') }} as t0
     on t0.token_address = s.token0_address
-  left join {{ ref('tokens') }} as t1
+  inner join {{ ref('tokens') }} as t1
     on t1.token_address = s.token1_address
-  where s.tx_hash not in ( -- remove gross mispriced swaps
-    '0x45306aade61a002fff4bf42b68edb48addfb821c0f5d373201f3ab33b8d4abb4',
-    '0x8c0b1638bf8f9e093b70d12faa5e639818e25902860fa1e0a6c422e6e3ccadfe'
-  )
 ),
 
 -- aggregate daily swap volumes between all pairs
 swaps_daily_agg as (
   select  
     s.block_date,
+    s.pool_address,
     s.token0_address,
     s.token0_symbol,
     s.token1_address,
@@ -49,269 +54,233 @@ swaps_daily_agg as (
     sum(s.amt0) as amt0,
     sum(s.amt1) as amt1
   from simpleswaps as s
-  group by 1,2,3,4,5
+  --where s.block_date >= '2022-02-27' and s.block_date < '2022-03-02'
+  group by 1,2,3,4,5,6
 ),
 
--- pull direct stable markets and calc the usd price
-swaps_d0 as (
-  select  
-    s.block_date,
-    s.token0_address,
-    s.token0_symbol,
-    sum(amt0) as amt0,
-    sum(amt1) as amt1,
-    sum(amt1) / sum(amt0) as usd_price
-  from swaps_daily_agg as s
-  inner join stables
-    on s.token1_address = stables.token_address
-  where s.token0_address not in (select token_address from stables)
-  group by 1,2,3
-),
 
--- pull direct stable markets and calc the usd price, from token1 to token0 (reversed)
-swaps_r0 as (
-  select  
-    s.block_date,
-    s.token1_address as token0_address,
-    s.token1_symbol as token0_symbol,
-    sum(amt1) as amt0,
-    sum(amt0) as amt1,
-    sum(amt0) / sum(amt1) as usd_price
-  from swaps_daily_agg as s
-  inner join stables
-    on s.token0_address = stables.token_address
-  where s.token1_address not in (select token_address from stables)
-  group by 1,2,3
-),
-
--- merge swaps d0 and r0
-swaps_0 as (
-  select
-    s.block_date,
-    s.token0_address,
-    s.token0_symbol,
-    sum(amt0) as amt0,
-    sum(amt1) as amt1,
-    sum(amt1) / sum(amt0) as usd_price
-  from
-    (
-      select * from swaps_d0
-      union all
-      select * from swaps_r0
-    ) as s
-    group by 1,2,3
-),
-
--- pull in the secondary markets - those that have a usd match in d0
-swaps_d1 as (
+consolidated_pairs as (
   select 
-    s.block_date,
-    s.token0_address,
-    s.token0_symbol,
-    sum(s.amt0) as amt0,
-    sum(s.amt1 * s0.usd_price) as amt1,
-    sum(s.amt1 * s0.usd_price) / sum(s.amt0) as usd_price
-    
-  from swaps_daily_agg as s
-  inner join swaps_0 s0
-    on s.token1_address = s0.token0_address
-        and s.block_date = s0.block_date
-  where s.token0_address not in (select token_address from stables)
-  group by 1,2,3
-),
-
--- pull in the secondary markets from reverse swaps - those that have a usd match in r0
-swaps_r1 as (
-  select 
-    s.block_date,
-    s.token1_address,
-    s.token1_symbol,
-    sum(s.amt1) as amt0,
-    sum(s.amt0 * s0.usd_price) as amt1,
-    sum(s.amt0 * s0.usd_price) / sum(s.amt1) as usd_price
-    
-  from swaps_daily_agg as s
-  inner join swaps_0 s0
-    on s.token0_address = s0.token0_address
-        and s.block_date = s0.block_date
-  where s.token1_address not in (select token_address from stables)
-  group by 1,2,3
-),
-
--- merge swaps d1 and r1
-swaps_1 as (
-  select
-    s.block_date,
-    s.token0_address,
-    s.token0_symbol,
-    sum(amt0) as amt0,
-    sum(amt1) as amt1,
-    sum(amt1) / sum(amt0) as usd_price
-  from
-    (
-      select * from swaps_d1
-      union all
-      select * from swaps_r1
-    ) as s
-    group by 1,2,3
-),
-
--- pull d0 and d1 together, consolidating volume from both tables
-swaps_d0_d1 as (
-  select 
+    (block_date || '-' || token_address) as key,
     block_date,
-    token0_address,
-    token0_symbol,
-    sum(amt0) as amt0,
-    sum(amt1) as amt1,
-    sum(amt1) / sum(amt0) as usd_price
-  from 
-    (select * from swaps_0
+    token_address,
+    token_symbol,
+    sum(amt) as amt,
+    pair_address,
+    pair_symbol,
+    sum(pair_amt) as pair_amt,
+    sum(pair_amt) / sum (amt) as price,
+    row_number() over (partition by block_date, token_address order by sum(amt) desc) as rank -- can't use rank() because of ties for 1st
+  from (
+    select 
+        block_date, 
+        token0_address as token_address, 
+        token0_symbol as token_symbol,
+        amt0 as amt,
+        token1_address as pair_address,
+        token1_symbol as pair_symbol,
+        amt1 as pair_amt
+    from swaps_daily_agg
     union all
-    select * from swaps_1) as t
-  group by 1,2,3
+    select 
+        block_date, 
+        token1_address as token_address, 
+        token1_symbol as token_symbol,
+        amt1 as amt,
+        token0_address as pair_address,
+        token0_symbol as pair_symbol,
+        amt0 as pair_amt
+    from swaps_daily_agg
+   ) --1372
+  group by 1,2,3,4,6,7
+  order by 4,2,5 desc
 ),
 
-/*
--- pull in tertiary markets - those that only have swaps through d1
-swaps_d2 as (
-  select 
-    s.block_date,
-    s.token0_address,
-    s.token0_symbol,
-    sum(s.amt0) as amt0,
-    sum(s.amt1 * sd0d1.usd_price) as amt1,
-    sum(s.amt1 * sd0d1.usd_price) / sum(s.amt0) as usd_price
-    
-  from swaps_daily_agg as s
-  inner join swaps_d1 sd1 -- filters only links through d1
-    on s.token1_address = sd1.token0_address
-        and s.block_date = sd1.block_date
-  left join swaps_d0_d1 sd0d1 -- pricing lookups with combined markets
-    on s.token1_address = sd0d1.token0_address
-        and s.block_date = sd0d1.block_date
-  where s.token0_address not in (select token_address from stables)
-  group by 1,2,3
+start_stables as (
+select distinct
+    (c.block_date || '-' || c.token_address) as key,
+    c.block_date,
+    s.token_address,
+    s.token_symbol,
+    1 as usd_price,
+    0 as volume_for_price,
+    null as price_pair_token,
+    null as price_pair_symbol,
+    'stables' as lookup_round
+from consolidated_pairs as c
+inner join stables as s
+  on c.token_address = s.token_address
 ),
 
--- pull d0,d1,d2 together, aggregating swap volume between them
-swaps_d0_d1_d2 as (
-  select 
-    block_date,
-    token0_address,
-    token0_symbol,
-    sum(amt0) as amt0,
-    sum(amt1) as amt1,
-    sum(amt1) / sum(amt0) as usd_price
-  from 
-    (select * from swaps_d0_d1
-    union all
-    select * from swaps_d2)
-  group by 1,2,3
+-- add WONE lookup
+wone_lookup as (
+select 
+    c.key,  
+    c.block_date,
+    c.token_address,
+    c.token_symbol,
+    sum(c.pair_amt) / sum(c.amt) as usd_price,
+    sum(c.amt) as volume_for_price,
+    null as price_pair_token,
+    null as price_pair_symbol,
+    'wONE' as lookup_round
+from consolidated_pairs as c
+  inner join start_stables as s
+    on c.pair_address = s.token_address
+where c.token_address = '0xcf664087a5bb0237a0bad6742852ec6c8d69a27a' -- WONE  
+group by 1,2,3,4,7,8
+  
+union all
+  select * from start_stables
 ),
 
--- pull in 4th tier markets - those that only have links through d2 (TODO - see if this actually has any matches)
-swaps_d3 as (
-  select 
-    s.block_date,
-    s.token0_address,
-    s.token0_symbol,
-    sum(s.amt0) as amt0,
-    sum(s.amt1 * sd0d1d2.usd_price) as amt1,
-    sum(s.amt1 * sd0d1d2.usd_price) / sum(s.amt0) as usd_price
-    
-  from swaps_daily_agg as s
-  inner join swaps_d2 sd2 -- filters only links through d1
-    on s.token1_address = sd2.token0_address
-        and s.block_date = sd2.block_date
-  left join swaps_d0_d1_d2 sd0d1d2 -- pricing lookups with combined markets
-    on s.token1_address = sd0d1d2.token0_address
-        and s.block_date = sd0d1d2.block_date
-  where s.token0_address not in (select token_address from stables)
-  group by 1,2,3
-),
+-- add lookups1a table for next round of matches, where top pair matches the wone_lookup table (wone + stables) (exclude keys that already exist in wone_lookup_table)
+lookups1a as (
+select 
+    c.key,  
+    c.block_date,
+    c.token_address,
+    c.token_symbol,
+    c.price * s.usd_price as usd_price,
+    c.amt as volume_for_price,
+    c.pair_address as pair_token_for_price,
+    c.pair_symbol as pair_symbol_for_price,
+    '1a' as lookup_round
+from consolidated_pairs as c
+  inner join wone_lookup as s
+    on c.pair_address = s.token_address
+    and c.block_date = s.block_date
+    and c.rank = 1
+where c.key not in (select key from wone_lookup)
+  
+union all
+  select * from wone_lookup
+), --112
 
-swaps_d0_d1_d2_d3 as (
-  select 
-    block_date,
-    token0_address,
-    token0_symbol,
-    sum(amt0) as amt0,
-    sum(amt1) as amt1,
-    sum(amt1) / sum(amt0) as usd_price
-  from 
-    (select * from swaps_d0_d1_d2
-    union all
-    select * from swaps_d3)
-  group by 1,2,3
-),
-*/
--- rename columns
-allswaps as (
-  select 
-    block_date,
-    token0_address as token_address,
-    token0_symbol as token_symbol,
-    amt0 as token_volume,
-    amt1 as usd_volume,
-    usd_price
-  --from swaps_d0_d1_d2_d3
-  from swaps_d0_d1
-),
+-- add lookups1b table for next round of matches, where top pair matches the lookups1a table (exclude keys that already exist in lookups1a table)
+lookups1b as (
+select 
+    c.key,  
+    c.block_date,
+    c.token_address,
+    c.token_symbol,
+    c.price * s.usd_price as usd_price,
+    c.amt as volume_for_price,
+    c.pair_address as pair_token_for_price,
+    c.pair_symbol as pair_symbol_for_price,
+    '1b' as lookup_round
+from consolidated_pairs as c
+  inner join lookups1a as s
+    on c.pair_address = s.token_address
+    and c.block_date = s.block_date
+    and c.rank = 1
+where c.key not in (select key from lookups1a)
+  
+union all
+  select * from lookups1a
+), --274
 
--- stable tokens with a price of 1 USD to complete the pricing table (from token0 position)
-usd_with_dates0 as (
-  select 
-    block_date,
-    token0_address,
-    token0_symbol,
-    sum(s.amt0) as token_volume,
-    sum(s.amt0) as usd_volume,
-    min(1) as usd_price
-  from swaps_daily_agg as s
-  inner join stables 
-    on s.token0_address = stables.token_address
-  group by 1,2,3
-),
+-- add lookups1c table for next round of matches, where top pair matches the lookups1b table (exclude keys that already exist in lookups1b table)
+lookups1c as (
+select 
+    c.key,  
+    c.block_date,
+    c.token_address,
+    c.token_symbol,
+    c.price * s.usd_price as usd_price,
+    c.amt as volume_for_price,
+    c.pair_address as pair_token_for_price,
+    c.pair_symbol as pair_symbol_for_price,
+    '1c' as lookup_round
+from consolidated_pairs as c
+  inner join lookups1b as s
+    on c.pair_address = s.token_address
+    and c.block_date = s.block_date
+    and c.rank = 1
+where c.key not in (select key from lookups1b)
+  
+union all
+  select * from lookups1b
+), -- 276
 
--- stable tokens with a price of 1 USD to complete the pricing table (from token1 position)
-usd_with_dates1 as (
-  select 
-    block_date,
-    token1_address,
-    token1_symbol,
-    sum(s.amt1) as token_volume,
-    sum(s.amt1) as usd_volume,
-    min(1) as usd_price
-  from swaps_daily_agg as s
-  inner join stables 
-    on s.token1_address = stables.token_address
-  group by 1,2,3
-),
+-- add lookups2a table for next round of matches, where 2nd most common pair matches the lookups1c table (exclude keys that already exist in lookups1c table)
+lookups2a as (
+select 
+    c.key,  
+    c.block_date,
+    c.token_address,
+    c.token_symbol,
+    c.price * s.usd_price as usd_price,
+    c.amt as volume_for_price,
+    c.pair_address as pair_token_for_price,
+    c.pair_symbol as pair_symbol_for_price,
+    '2a' as lookup_round
+from consolidated_pairs as c
+  inner join lookups1c as s
+    on c.pair_address = s.token_address
+    and c.block_date = s.block_date
+    and c.rank = 2
+where c.key not in (select key from lookups1c)
+  
+union all
+  select * from lookups1c
+), -- 278
 
--- combine stables volume tables
-usd_with_dates_combined as (
-  select 
-    block_date,
-    token0_address as token_address,
-    token0_symbol as token_symbol,
-    sum(token_volume) as token_volume,
-    sum(token_volume) as usd_volume,
-    min(1) as usd_price
-  from 
-    (
-      select * from usd_with_dates0
-      union all
-      select * from usd_with_dates1
-    )
-  group by 1,2,3
-),
+-- add lookups2b table for next round of matches, where 2nd most common pair matches the lookups2a table (exclude keys that already exist in lookups2a table)
+lookups2b as (
+select 
+    c.key,  
+    c.block_date,
+    c.token_address,
+    c.token_symbol,
+    c.price * s.usd_price as usd_price,
+    c.amt as volume_for_price,
+    c.pair_address as pair_token_for_price,
+    c.pair_symbol as pair_symbol_for_price,
+    '2b' as lookup_round
+from consolidated_pairs as c
+  inner join lookups2a as s
+    on c.pair_address = s.token_address
+    and c.block_date = s.block_date
+    and c.rank = 1
+where c.key not in (select key from lookups2a)
+  
+union all
+  select * from lookups2a
+), -- 278
 
--- combine pricing for all swaps with stable token volume
+-- IF needed, additional lookup rounds could be added, e.g.:
+-- TODO: add lookups2c table for next round of matches, where 2nd most common pair matches the lookups2b table (exclude keys that already exist in lookups2b table)
+-- TODO: add lookups3a table for next round of matches, where 3rd most common pair matches the lookups2c table (exclude keys that already exist in lookups2c table)
+-- TODO: add lookups3b table for next round of matches, where 3rd most common pair matches the lookups3a table (exclude keys that already exist in lookups3a table)
+-- TODO: add lookups3c table for next round of matches, where 3rd most common pair matches the lookups3b table (exclude keys that already exist in lookups3b table)
+-- TODO: remove date filter from swaps_daily_agg
+
 final as (
-  select * from usd_with_dates_combined
-  union all
-  select * from allswaps
+select 
+  l.block_date,
+  l.token_address,
+  l.token_symbol,
+  l.usd_price,
+  l.usd_price * sum(c.amt) as usd_volume,
+  sum(c.amt) as token_volume,
+  l.pair_token_for_price,
+  l.pair_symbol_for_price,
+  l.volume_for_price,
+  l.key,
+  l.lookup_round
+from lookups2b as l
+left join consolidated_pairs as c 
+    on l.key = c.key
+group by 1,2,3,4,7,8,9,10,11
+order by l.token_symbol, l.block_date asc
 )
+
+-- the check see if any are missing from final (should be empty):
+--select * from final 
+--where key not in (select distinct key from consolidated_pairs);
+
+--select * from final;  
+--select distinct key from consolidated_pairs; 
 
 select * from final
