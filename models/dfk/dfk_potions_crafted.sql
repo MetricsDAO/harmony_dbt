26c2133f033174577e0bbae660c8b1acfdaed9b4
Anton Yip <anton_yip@hotmail.com>
2022-03-12 12:06:24 +0800

    feat: dfk_potions_crafted

--- models/dfk/dfk_potions_crafted.sql
+++ models/dfk/dfk_potions_crafted.sql
@@ -8,18 +8,23 @@
         ) 
 }}
 
-with 
-
+with
+txs as (
+    select
+        *
+    from {{ ref('txs') }}
+    where {{ incremental_load_filter("block_timestamp") }}
+),
 create_potions as (
     select
-        block_timestamp,
         tx_hash,
+        block_timestamp,
         from_address as crafter,
         concat('0x',substr(data,11+24,40)) as potion_type,
         java_hextoint(substr(data,11+64,64)) as potions_crafted
-    from {{ ref('txs') }}
-    where to_address = '0x87cba8f998f902f2fff990effa1e261f35932e57'
-        and substr(data,0,10) = '0xa28beda3'
+    from txs
+    where to_address = '0x87cba8f998f902f2fff990effa1e261f35932e57' -- alchemist address
+        and substr(data,0,10) = '0xa28beda3' -- create potion
 )
 
 select * from create_potions
