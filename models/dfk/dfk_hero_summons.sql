2782614cf49d2af2949564d11a855f38c2e567e1
Anton Yip <anton_yip@hotmail.com>
2022-03-14 08:35:43 +0800

    revamp hero summons

--- models/dfk/dfk_hero_summons.sql
+++ models/dfk/dfk_hero_summons.sql
@@ -26,35 +26,28 @@ txs as (
 
 crystal_to_hero_summons as (
     select  
-        block_timestamp,
-        tx_hash,
-        substr(data,11) as crystal_id,
-        from_address as summoneer
+        txs.block_timestamp,
+        txs.tx_hash,
+        substr(txs.data,11) as crystal_id,
+        txs.from_address as summoneer,
+        logs.event_inputs:tokenId as hero_id
     from txs
-    where 1=1
-        and (to_address = '0x65dea93f7b886c33a78c10343267dd39727778c2' or to_address = '0xf4d3ae202c9ae516f7eb1db5aff19bf699a5e355')
-        and substr(data,0,10) = '0x690e7c09'
+    left join logs on txs.tx_hash = logs.tx_hash and logs.event_name = 'Transfer'
+    where substr(txs.data,0,10) = '0x690e7c09' -- open
+        and status = TRUE
+        and (
+                txs.to_address = '0x65dea93f7b886c33a78c10343267dd39727778c2' -- Summoning Contract
+                or txs.to_address = '0xf4d3ae202c9ae516f7eb1db5aff19bf699a5e355' -- Summoning Contract 2
+            )
 ),
-
-generation as (
-    select 
-        tx_hash,
-        event_inputs:tokenId as hero_id
-    from logs
-    where tx_hash in (select tx_hash from crystal_to_hero_summons)
-        and event_name = 'Transfer'
-),
-
 final as (
     select 
-        g.tx_hash,
+        tx_hash,
         hero_id,
         concat('0x',crystal_id) as crystal_id,
-        s.block_timestamp,
+        block_timestamp,
         summoneer
-    from generation g
-    left join crystal_to_hero_summons s
-        on s.tx_hash = g.tx_hash
+    from crystal_to_hero_summons s
 )
 
 select * from final
