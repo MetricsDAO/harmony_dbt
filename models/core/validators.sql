bbbbc948442f63f54494dadce1d6738582ff294b
Anton Yip <anton_yip@hotmail.com>
2022-03-25 00:01:26 +0800

    fixes

--- models/core/validators.sql
+++ models/core/validators.sql
@@ -1,22 +1,23 @@
 {{
-	config(
-		materialized='incremental',
-		unique_key='u_key',
-		tags=['core'],
-		cluster_by=['day_date']
-	)
+    config(
+        materialized='incremental',
+        unique_key='u_key',
+        tags=['core'],
+        cluster_by=['day_date']
+    )
 }}
 
 with delegators_incremental as (
-	select
-	    *
-	from {{ ref('stg_delegators') }}
-	where {{ incremental_load_filter('ingest_timestamp') }}
+    select
+        *
+    from {{ ref('stg_delegators') }}
+    where {{ incremental_load_filter_2('ingest_timestamp', 'day_date') }}
+    
 ),
 
 latest_day_timestamps as (
     select
-	day_date,
+        day_date,
         validator_address,
         max(ingest_timestamp) as ingest_timestamp
     from delegators_incremental
@@ -29,8 +30,8 @@ latest_day_flat_delegations as (
         ingest_timestamp,
         validator_address
     from delegators_incremental
-	natural join latest_day_timestamps
-	inner join lateral flatten(input => delegations)
+    natural join latest_day_timestamps
+    inner join lateral flatten(input => delegations)
 ),
 
 rewards as (
@@ -48,7 +49,7 @@ undelegations as (
         validator_address,
         sum(ifnull(flat_undelegations.value:amount, 0)) as amount
     from latest_day_flat_delegations
-	inner join lateral flatten(input => value:undelegations, outer => true) flat_undelegations
+    inner join lateral flatten(input => value:undelegations, outer => true) flat_undelegations
     group by 1, 2
 ),
 
@@ -58,33 +59,34 @@ totals as (
         rewards.validator_address as validator_address,
         rewards.amount as total_one_rewarded,
         undelegations.amount as total_one_undelegated
-    from rewards join undelegations
-    on rewards.ingest_timestamp = undelegations.ingest_timestamp
-    and rewards.validator_address = undelegations.validator_address
+    from rewards
+    join undelegations
+        on rewards.ingest_timestamp = undelegations.ingest_timestamp
+        and rewards.validator_address = undelegations.validator_address
 ),
 
 validators as (
-	select
-		day_date,
-		delegators_incremental.validator_address as validator_address,
-		js_onetohex(delegators_incremental.validator_address) as validator_hex_address,
-		validator_identity,
-		active_status,
-		booted_status,
-		total_delegation as total_one_delegated,
-		ifnull(total_one_rewarded, 0) as total_one_rewarded,
-		ifnull(total_one_undelegated, 0) as total_one_undelegated
-	from delegators_incremental
-	join totals 
-	on delegators_incremental.ingest_timestamp = totals.ingest_timestamp
-	and delegators_incremental.validator_address = totals.validator_address
+    select
+        day_date,
+        delegators_incremental.validator_address as validator_address,
+        js_onetohex(delegators_incremental.validator_address) as validator_hex_address,
+        validator_identity,
+        active_status,
+        booted_status,
+        total_delegation as total_one_delegated,
+        ifnull(total_one_rewarded, 0) as total_one_rewarded,
+        ifnull(total_one_undelegated, 0) as total_one_undelegated
+    from delegators_incremental
+    join totals 
+        on delegators_incremental.ingest_timestamp = totals.ingest_timestamp
+        and delegators_incremental.validator_address = totals.validator_address
 ),
 
 final as (
-	select
-		concat_ws('-', to_char(day_date, '%Y%m%d'), validator_address) as u_key,
-		*
-	from validators
+    select
+        concat_ws('-', to_char(day_date, '%Y%m%d'), validator_address) as u_key,
+        *
+    from validators
 )
 
 select * from final
