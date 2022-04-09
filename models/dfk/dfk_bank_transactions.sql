0fed71dc76686d897f3879b47d24d00043e93914
Anton Yip <anton_yip@hotmail.com>
2022-04-09 12:30:20 +0800

    formatting fixes + jewel_adjusted

--- models/dfk/dfk_bank_transactions.sql
+++ models/dfk/dfk_bank_transactions.sql
@@ -1,16 +1,24 @@
 {{
-	config(
-		materialized='incremental',
-		unique_key='tx_hash',
-		tags=['dfk'],
-		cluster_by=['block_timestamp']
-	)
+    config(
+        materialized='incremental',
+        unique_key='tx_hash',
+        tags=['dfk'],
+        cluster_by=['block_timestamp']
+    )
 }}
 
-with incremental_txs as (
-	select *
+with
+
+incremental_txs as (
+    select *
     from {{ ref("txs") }}
-	where {{ incremental_load_filter("block_timestamp") }}
+    where {{ incremental_load_filter("block_timestamp") }}
+),
+
+incremental_logs as (
+    select *
+    from {{ ref("logs") }}
+    where {{ incremental_load_filter("block_timestamp") }}
 ),
 
 banking_txs as (
@@ -25,8 +33,8 @@ banking_txs as (
         end as type
     from incremental_txs
     where to_address = '0xa9ce83507d872c5e1273e745abcfda849daa654f'
-    and (data like '0xa59f3e0c%' or data like '0x67dfd4c9%')
-    and status = true
+        and (data like '0xa59f3e0c%' or data like '0x67dfd4c9%')
+        and status = true
 ),
 
 token_amounts as (
@@ -35,8 +43,8 @@ token_amounts as (
         sum(case evm_contract_address when '0x72cb10c6bfa5624dd07ef608027e366bd690048f' then event_inputs:value end) as jewel_amount,
         sum(case evm_contract_address when '0xa9ce83507d872c5e1273e745abcfda849daa654f' then event_inputs:value end) as xjewel_amount
     from banking_txs
-    join {{ ref("logs") }} logs
-	on banking_txs.tx_hash = logs.tx_hash
+    join incremental_logs as logs
+        on banking_txs.tx_hash = logs.tx_hash
     where event_name = 'Transfer'
     group by 1
 ),
@@ -48,11 +56,11 @@ final as (
         native_from_address,
         from_address,
         type,
-        jewel_amount,
-        xjewel_amount
+        jewel_amount / pow(10,18) as jewel_amount,
+        xjewel_amount / pow(10,18) as xjewel_amount
     from banking_txs
     join token_amounts
-	on banking_txs.tx_hash = token_amounts.tx_hash
+        on banking_txs.tx_hash = token_amounts.tx_hash
 )
 
 select * from final
