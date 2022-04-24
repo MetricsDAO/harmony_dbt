2c49f0305599b1419e78e8a951b6427e415844d7
D0R11 <dorii0010@protonmail.com>
2022-04-24 17:58:21 +0800

    applied changes regarding the style guide

--- models/dfk/dfk_seed_liquidity_actions.sql
+++ models/dfk/dfk_seed_liquidity_actions.sql
@@ -35,35 +35,37 @@ txs as (
             in ('0x02751cec','0xf305d719','0xbaa2abde','0xe8e33700')
 ),
 
-main_table as (
+final_table as (
     select
-        logs.log_id
-        ,logs.block_id
-        ,logs.block_timestamp
-        ,logs.tx_hash
-        ,logs.event_inputs:amount0 / pow(10, t0.decimals) 
-            as amount_0
-        ,logs.event_inputs:amount1 / pow(10, t1.decimals)
-            as amount_1
-        ,logs.native_contract_address 
-            as one_lp_address
-        ,logs.evm_contract_address 
-            as eth_lp_address
-        ,liquidity_pools.pool_name
-        ,liquidity_pools.token0 
-            as token0_address
-        ,liquidity_pools.token1 
-            as token1_address
-        ,t0.token_name 
-            as token0_name
-        ,t1.token_name 
-            as token1_name
-        ,case
+        logs.log_id,
+        logs.block_id,
+        logs.block_timestamp,
+        logs.tx_hash,
+        logs.event_inputs:amount0 / pow(10, t0.decimals) 
+            as amount_0,
+        logs.event_inputs:amount1 / pow(10, t1.decimals)
+            as amount_1,
+        logs.native_contract_address 
+            as one_lp_address,
+        logs.evm_contract_address 
+            as eth_lp_address,
+        liquidity_pools.pool_name,
+        liquidity_pools.token0 
+            as token0_address,
+        liquidity_pools.token1 
+            as token1_address,
+        t0.token_name 
+            as token0_name,
+        t1.token_name 
+            as token1_name,
+        
+        case
             when event_name = 'Mint' 
                 then 'ADD_LIQUIDITY' 
             when event_name = 'Burn' 
                 then 'REMOVE_LIQUIDITY'
-            end as action
+        end as action
+
     from logs
         left join liquidity_pools 
             on logs.evm_contract_address = liquidity_pools.pool_address
@@ -73,8 +75,8 @@ main_table as (
             on liquidity_pools.token1 = t1.token_address
     where 1=1
         and tx_hash in (select tx_hash from txs)
-        and (event_name = 'Mint' 
-             or event_name = 'Burn')
+        and (event_name = 'Mint' or 
+             event_name = 'Burn')
 )
 
-select * from main_table
+select * from final_table
