e4a5d8cc69a3f1caaf3f27ec9ba17b40c0262eb7
Anton Yip <anton_yip@hotmail.com>
2022-04-26 05:54:53 +0800

    sync

--- models/core/liquidity_pools.sql
+++ models/core/liquidity_pools.sql
@@ -16,39 +16,36 @@ dfk_lp as (
 ),
 
 src_logs_lp as (
-select 
-    event_inputs:pair::string as pool_address,
-    '' as pool_name,
-    event_inputs:token0::string as token0,
-    event_inputs:token1::string as token1
-from {{ ref('logs') }} 
-where event_name = 'PairCreated'
-  ),
-  
-  logs_lp as (
-  select 
-    pool_address,
-    t0.token_symbol || '-' || t1.token_symbol || ' LP' as pool_name, -- "TOKEN0-TOKEN1 LP"
-    token0,
-    token1
-  from src_logs_lp p
-  inner join {{ ref('tokens') }} as t0
-    on p.token0 = t0.token_address
-  inner join {{ ref('tokens') }} as t1
-    on p.token1 = t1.token_address
-  where p.pool_address not in
-    (select pool_address from dfk_lp)
-  ),
+    select 
+        event_inputs:pair::string as pool_address,
+        '' as pool_name,
+        event_inputs:token0::string as token0,
+        event_inputs:token1::string as token1
+    from {{ ref('logs') }} 
+    where event_name = 'PairCreated'
+),
 
-  backfill_from_swaps as (
-      select
-          *
-      from {{ ref('backfill_pools_data') }}
-      where pool_address not in 
-        (select pool_address from logs_lp)
-        and pool_address not in 
-        (select pool_address from dfk_lp)
-  ),
+logs_lp as (
+    select
+        pool_address,
+        t0.token_symbol || '-' || t1.token_symbol || ' LP' as pool_name, -- "TOKEN0-TOKEN1 LP"
+        token0,
+        token1
+    from src_logs_lp p
+    inner join {{ ref('tokens') }} as t0
+        on p.token0 = t0.token_address
+    inner join {{ ref('tokens') }} as t1
+        on p.token1 = t1.token_address
+    where p.pool_address not in (select pool_address from dfk_lp)
+),
+
+backfill_from_swaps as (
+    select
+        *
+    from {{ ref('backfill_pools_data') }}
+    where pool_address not in (select pool_address from logs_lp)
+        and pool_address not in (select pool_address from dfk_lp)
+),
 
 -- this is an example of adding new protocols
 tranq_lp as (
