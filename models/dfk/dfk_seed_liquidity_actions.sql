d5a406cb56d442fcaaff54c07a9d2110e18db939
Anton Yip <anton_yip@hotmail.com>
2022-05-07 06:36:26 +0800

    fixes

--- models/dfk/dfk_seed_liquidity_actions.sql
+++ models/dfk/dfk_seed_liquidity_actions.sql
@@ -12,7 +12,13 @@ with
 logs as (
     select * 
     from {{ ref('logs') }}
-    where {{ incremental_load_filter("block_timestamp") }}
+    where {{ incremental_load_filter("ingested_at") }}
+),
+
+txs_incremental as (
+    select * 
+    from {{ ref('txs') }}
+    where {{ incremental_load_filter("ingested_at") }}
 ),
 
 liquidity_pools as (
@@ -28,9 +34,8 @@ tokens as (
 txs as (
     select 
         tx_hash 
-    from {{ ref('txs') }}
-    where {{ incremental_load_filter("block_timestamp") }}
-        and to_address = '0x24ad62502d1c652cc7684081169d04896ac20f30' -- UniswapV2Router address
+    from txs_incremental
+    where to_address = '0x24ad62502d1c652cc7684081169d04896ac20f30' -- UniswapV2Router address
         and substr(data,0,10) 
             in ('0x02751cec', -- RemoveLiquidityETH
                 '0xf305d719', -- AddLiquidityETH
@@ -43,6 +48,7 @@ final_table as (
         logs.log_id,
         logs.block_id,
         logs.block_timestamp,
+        logs.ingested_at,
         logs.tx_hash,
         logs.event_inputs:amount0 / pow(10, t0.decimals) 
             as amount_0,
@@ -61,7 +67,6 @@ final_table as (
             as token0_name,
         t1.token_name 
             as token1_name,
-        
         case
             when event_name = 'Mint' 
                 then 'ADD_LIQUIDITY' 
