1ca3ef051db98c839dc7a5b787c2c668e03fadff
Geewynn <ekainugodwin@gmail.com>
2022-06-11 14:50:27 +0100

    updated doc and formatted script

--- models/metrics/metric_erc1155_mint_burn_count.sql
+++ models/metrics/metric_erc1155_mint_burn_count.sql
@@ -1,68 +1,92 @@
-{{ 
-    config(
-        materialized='incremental',
-        tags=['metrics'],
-        incremental_strategy = 'delete+insert',
-        unique_key=['day_date'],
-        cluster_by=['day_date']
-    )
-}}
+{{ config(
+    materialized = 'incremental',
+    tags = ['metrics'],
+    incremental_strategy = 'delete+insert',
+    unique_key = ['day_date', 'evm_contract_address'],
+    cluster_by = ['day_date']
+) }}
 
-with
-logs as (
-select
-    block_timestamp,
-    ingested_at,
-    event_name,
-    evm_contract_address,
-    topics,
-    event_inputs:_from::string as mint,
-    event_inputs:_to::string as burn
-from {{ ref("logs") }}
-where {{ incremental_last_x_days('block_timestamp', '3')}}
-),
-events as (
-select
-    block_timestamp,
-    ingested_at,
-    event_name,
-    evm_contract_address,
-    mint,
-    burn
-from logs
-where topics[0] = '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62'
-),
+WITH logs AS (
 
-mint as (
-select
-    evm_contract_address,
-    date_trunc('day', block_timestamp) as day_date,
-    count(1) as daily_count
-from
-    events
-where mint='0x0000000000000000000000000000000000000000'
-group by 1,2
+    SELECT
+        block_timestamp,
+        ingested_at,
+        event_name,
+        evm_contract_address,
+        topics,
+        event_inputs :_from :: STRING AS mint,
+        event_inputs :_to :: STRING AS burn
+    FROM
+        {{ ref("logs") }}
+    WHERE
+        {{ incremental_last_x_days(
+            'block_timestamp',
+            '3'
+        ) }}
 ),
-
-burn as (
-select
-    evm_contract_address,
-    date_trunc('day', block_timestamp) as day_date,
-    count(1) as daily_count
-from
-    events
-where burn='0x0000000000000000000000000000000000000000'
-group by 1,2
+events AS (
+    SELECT
+        block_timestamp,
+        ingested_at,
+        event_name,
+        evm_contract_address,
+        mint,
+        burn
+    FROM
+        logs
+    WHERE
+        topics [0] = '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62'
 ),
-
-final as (
-select 
-  nvl(m.evm_contract_address, b.evm_contract_address) as evm_contract_address,
-  nvl(m.day_date, b.day_date) as day_date,
-  m.daily_count as tokens_minted,
-  b.daily_count as tokens_burned
-  from mint m 
-  left join burn b on m.day_date = b.day_date
+mint AS (
+    SELECT
+        evm_contract_address,
+        DATE_TRUNC(
+            'day',
+            block_timestamp
+        ) AS day_date,
+        COUNT(1) AS daily_count
+    FROM
+        events
+    WHERE
+        mint = '0x0000000000000000000000000000000000000000'
+    GROUP BY
+        1,
+        2
+),
+burn AS (
+    SELECT
+        evm_contract_address,
+        DATE_TRUNC(
+            'day',
+            block_timestamp
+        ) AS day_date,
+        COUNT(1) AS daily_count
+    FROM
+        events
+    WHERE
+        burn = '0x0000000000000000000000000000000000000000'
+    GROUP BY
+        1,
+        2
+),
+FINAL AS (
+    SELECT
+        NVL(
+            m.evm_contract_address,
+            b.evm_contract_address
+        ) AS evm_contract_address,
+        NVL(
+            m.day_date,
+            b.day_date
+        ) AS day_date,
+        m.daily_count AS tokens_minted,
+        b.daily_count AS tokens_burned
+    FROM
+        mint m
+        LEFT JOIN burn b
+        ON m.day_date = b.day_date
 )
-
-select * from final
+SELECT
+    *
+FROM
+    FINAL
